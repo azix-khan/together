9b64c559e89fefac9709ec14e309b50c8ab304fe
azix-khan <azixkhan.55@gmail.com>
2024-10-25 12:48:46 +0500

    setted up firebase and auth feature

---
+++ lib/features/auth/domain/repository/auth_repo.dart
@@ -0,0 +1,15 @@
+/*
+
+Auth reository - Outlines the posible auth operations for this app
+
+*/
+
+import 'package:together/features/auth/domain/entities/app_user.dart';
+
+abstract class AuthRepo {
+  Future<AppUser?> loginWithEmailPassword(String email, String password);
+  Future<AppUser?> registerWithEmailPassword(
+      String name, String email, String password);
+  Future<void> logout();
+  Future<AppUser?> getCurrentUser();
+}
