c76559e26bf806add4b8697b9a8a994ef44a7f7f
azix-khan <azixkhan.55@gmail.com>
2024-10-29 23:29:49 +0500

    post cubit done

---
+++ lib/features/post/presentation/cubits/post_cubit.dart
@@ -0,0 +1,65 @@
+import 'dart:typed_data';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:together/features/post/domain/entities/post.dart';
+import 'package:together/features/post/domain/repository/post_repo.dart';
+import 'package:together/features/post/presentation/cubits/post_states.dart';
+import 'package:together/features/storage/domain/storage_repo.dart';
+
+class PostCubit extends Cubit<PostState> {
+  final PostRepo postRepo;
+  final StorageRepo storageRepo;
+
+  PostCubit({
+    required this.postRepo,
+    required this.storageRepo,
+  }) : super(PostsInitial());
+
+  // create a new post
+  Future<void> createPost(Post post,
+      {String? imagePath, Uint8List? imageBytes}) async {
+    String? imageUrl;
+
+    try {
+      // handle image upload for mobile platform (using file path)
+      if (imagePath != null) {
+        emit(PostUploading());
+        imageUrl =
+            await storageRepo.uploadProfileImageMobile(imagePath, post.id);
+      }
+
+      // handle image upload for web platform (using file bytes)
+      else if (imageBytes != null) {
+        emit(PostUploading());
+        imageUrl = await storageRepo.uploadProfileImageWeb(imageBytes, post.id);
+      }
+
+      // give image url to post
+      final newPost = post.copyWith(imageUrl: imageUrl);
+
+      // create post in the backend
+      postRepo.createPost(newPost);
+    } catch (e) {
+      emit(PostsError("Failed to create post: $e"));
+    }
+  }
+
+  // fetch all posts
+  Future<void> fetchAllPosts() async {
+    try {
+      emit(PostsLoading());
+      final posts = await postRepo.fetchAllPosts();
+      emit(PostsLoaded(posts));
+    } catch (e) {
+      emit(PostsError("Faild to fetch posts: $e"));
+    }
+  }
+
+  // delete a post
+  Future<void> deletePost(String postId) async {
+    try {
+      await postRepo.deletePost(postId);
+    } catch (e) {
+      emit(PostsError("Faild to delete post: $e"));
+    }
+  }
+}
