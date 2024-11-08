1e16c944899671470910627338d8288dfb33ba30
azix-khan <azixkhan.55@gmail.com>
2024-11-08 22:16:12 +0500

    now working comments

---
+++ lib/features/post/domain/entities/comment.dart
@@ -0,0 +1,17 @@
+class Comment {
+  final String id;
+  final String postId;
+  final String userId;
+  final String userName;
+  final String text;
+  final DateTime timestamp;
+
+  Comment({
+    required this.id,
+    required this.postId,
+    required this.userId,
+    required this.userName,
+    required this.text,
+    required this.timestamp,
+  });
+}
