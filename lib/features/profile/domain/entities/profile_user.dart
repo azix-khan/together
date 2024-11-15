c906a73d3c0f66856c9fded389b83eb9952746e3
azix-khan <azixkhan.55@gmail.com>
2024-11-15 11:10:57 +0500

    working on following and followers feature

--- lib/features/profile/domain/entities/profile_user.dart
+++ lib/features/profile/domain/entities/profile_user.dart
@@ -3,6 +3,8 @@ import 'package:together/features/auth/domain/entities/app_user.dart';
 class ProfileUser extends AppUser {
   final String bio;
   final String profileImageUrl;
+  final List<String> followers;
+  final List<String> following;
 
   ProfileUser({
     required super.uid,
@@ -10,16 +12,25 @@ class ProfileUser extends AppUser {
     required super.email,
     required this.bio,
     required this.profileImageUrl,
+    required this.followers,
+    required this.following,
   });
 
   // method to update user profile
-  ProfileUser copyWith({String? newBio, String? newProfileImageUrl}) {
+  ProfileUser copyWith({
+    String? newBio,
+    String? newProfileImageUrl,
+    List<String>? newFollowers,
+    List<String>? newFollowing,
+  }) {
     return ProfileUser(
       uid: uid,
       name: name,
       email: email,
       bio: newBio ?? bio,
       profileImageUrl: newProfileImageUrl ?? profileImageUrl,
+      followers: newFollowers ?? followers,
+      following: newFollowing ?? following,
     );
   }
 
@@ -32,6 +43,8 @@ class ProfileUser extends AppUser {
       'email': email,
       'bio': bio,
       'profileImageUrl': profileImageUrl,
+      'follower': followers,
+      'following': following,
     };
   }
 
@@ -43,6 +56,8 @@ class ProfileUser extends AppUser {
       email: json['email'],
       bio: json['bio'] ?? '',
       profileImageUrl: json['profileImageUrl'] ?? '',
+      followers: List<String>.from(json['followers'] ?? []),
+      following: List<String>.from(json['following'] ?? []),
     );
   }
 }
